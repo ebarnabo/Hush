ebe8e7f7d5dac1ea4077d0aa01f1703f865a905c
ebarnabo <edwinbarnabot@gmail.com>
2023-05-31 21:01:07 -0400

    SRC folders
    SRC

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- Le reste du contenu de app.vue -->
+    <inscription/>
+</template>
+
+<script setup>
+import inscription from './components/inscription.vue';
+</script>
+
+<style scoped lang="scss">
+  @import './assets/style.scss'
+
+</style>
+
+export default {
+  name: 'App',
+}
